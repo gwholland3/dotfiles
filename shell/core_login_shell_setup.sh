a0fe681c831432b845720e5de29c4b8fa4641700
Grant Holland <gwholland3@gmail.com>
2026-03-21 17:22:21 -0700

    Add guard for fzf version

--- shell/core_login_shell_setup.sh
+++ shell/core_login_shell_setup.sh
@@ -7,11 +7,15 @@ fi
 
 # Activate fzf shell integration (adds key bindings).
 # See https://junegunn.github.io/fzf/shell-integration/
-if [ -n "$BASH_VERSION" ]; then
-   FZF_CTRL_R_COMMAND= eval "$(fzf --bash)"
-fi
-if [ -n "$ZSH_VERSION" ]; then
-   FZF_CTRL_R_COMMAND= source <(fzf --zsh)
+# This functionality only exists in fzf version 0.48.0 and later.
+fzf_version=$(fzf --version | cut -d' ' -f1)
+if printf '%s\n' "0.48.0" "$fzf_version" | sort -V -C; then
+   if [ -n "$BASH_VERSION" ]; then
+      FZF_CTRL_R_COMMAND= eval "$(fzf --bash)"
+   fi
+   if [ -n "$ZSH_VERSION" ]; then
+      FZF_CTRL_R_COMMAND= source <(fzf --zsh)
+   fi
 fi
 
 if [ -f ~/.shell_setup/shell_aliases/shell_aliases.sh ]; then
